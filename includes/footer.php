62d95e611faf7730be2854239f0743ec85d6239c
Elaf Nadaf <elafnadaf36@gmail.com>
2025-08-02 22:35:30 +0530

    Created footer

---
+++ includes/footer.php
@@ -0,0 +1,14 @@
+<footer>
+  <div class="footer-container">
+    <div class="footer-info">
+      <h3>Contact Us</h3>
+      <p>Email: support@espressoease.com</p>
+      <p>Phone: +91 12345 67890</p>
+    </div>
+    <div class="footer-links">
+      <a href="/">Privacy Policy</a>
+      <a href="/">Terms of Service</a>
+    </div>
+    <p class="copyright">&copy; 2025 EspressoEase. All rights reserved.</p>
+  </div>
+</footer>
