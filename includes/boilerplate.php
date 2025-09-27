92ad748093d356d36fb8306718f4044fc1554933
Elaf Nadaf <elafnadaf36@gmail.com>
2025-09-27 19:10:06 +0530

    Added Boilerplate code

---
+++ includes/boilerplate.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>EspressoEase</title>
+
+  <!-- Font Awesome Link -->
+
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap" rel="stylesheet">
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+
+  <!-- CSS -->
+  <link rel="stylesheet" href="/EspressoEase-v2/assets/css/style.css">
+</head>
+<body>
+  <?php include (__DIR__ . "/navbar.php"); ?>
+
+  <script src="/EspressoEase-v2/assets/js/navbar.js"></script>
+</body>
+</html>
